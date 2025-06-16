8a54ff22c93bf673c88e53344e4122d997c57738
Dilshan099987 <ashinsadilshan494@gmail.com>
2025-06-16 11:07:58 +0530

    Update animegirl5.js

--- plugins/animegirl5.js
+++ plugins/animegirl5.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { cmd, commands } = require('../command');
 
 cmd({
-    pattern: "animegirl3",
+    pattern: "animegirl5",
     desc: "Fetch a random anime girl image.",
     category: "fun",
     react: "👧",
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *THENUWA XMD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SILENT-KILLER-MD BY SILENT-KILLER469*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *THENUWA XMD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SILENT-KILLEF-MD BY SILENTKILLER469*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
